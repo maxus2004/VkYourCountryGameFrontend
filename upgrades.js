5c48861635b76671cb2689a1ef8912f4748c5831
Дорошин Максим <maxus2004123@gmail.com>
2021-04-09 23:17:10 +0300

    text

--- upgrades.js
+++ upgrades.js
@@ -1,19 +1,19 @@
 var jobs = [
-    { name: 'Сдавать металлолом', cost: 0, reward: 50, rewardDelay: 0, repeating: false, maxAmount: 1 },
-    { name: 'Попрошайничать', cost: 0, reward: 50, rewardDelay: 0, repeating: false, maxAmount: 1 },
-    { name: 'Работать на складе', cost: 0, reward: 5000, rewardDelay: 3, repeating: false, maxAmount: 1 },
-    { name: 'Продавать мороженное', cost: 500, reward: 2000, rewardDelay: 0, repeating: false, maxAmount: 1 },
-    { name: 'Открыть магазин', cost: 50000, reward: 200000, rewardDelay: 30, repeating: true, maxAmount: 20 },
-    { name: 'Стать президентом', cost: 100000000, reward: 10000000000, rewardDelay: 365, repeating: false, maxAmount: 1 },
+    { name: 'Сдавать металлолом', cost: 0, reward: 50, rewardDelay: 0, repeating: false },
+    { name: 'Попрошайничать', cost: 0, reward: 50, rewardDelay: 0, repeating: false },
+    { name: 'Работать на складе', cost: 0, reward: 5000, rewardDelay: 3, repeating: false },
+    { name: 'Продавать мороженное', cost: 500, reward: 2000, rewardDelay: 0, repeating: false },
+    { name: 'Открыть магазин', cost: 50000, reward: 200000, rewardDelay: 30, repeating: true },
+    { name: 'Стать президентом', cost: 100000000, reward: 10000000000, rewardDelay: 365, repeating: false },
 ]
 
-function numToDays(num) {
+function numDaysToText(num) {
     if (num % 10 == 1) {
-        return 'день';
+        return num + ' день';
     } else if (num % 10 >= 2 && num % 10 <= 4) {
-        return 'дня';
+        return num + ' дня';
     } else {
-        return 'дней';
+        return num + ' дней';
     }
 }
 
@@ -33,10 +33,14 @@ function loadJobs() {
         jobNode.appendChild(jobCostNode);
         var jobRewardNode = document.createElement('p');
         jobRewardNode.className = 'jobReward';
-        if (job.rewardDelay > 0)
-            jobRewardNode.innerText = 'Выгода: ' + job.reward + '₽ через ' + job.rewardDelay + ' ' + numToDays(job.rewardDelay);
-        else
-            jobRewardNode.innerText = 'Выгода: ' + job.reward + '₽';
+        if (job.repeating) {
+            jobRewardNode.innerText = 'Выгода: ' + job.reward + '₽ каждый ' + job.rewardDelay + 'й день';
+        } else {
+            if (job.rewardDelay > 0)
+                jobRewardNode.innerText = 'Выгода: ' + job.reward + '₽ через ' + job.rewardDelay + ' ' + numDaysToText(job.rewardDelay);
+            else
+                jobRewardNode.innerText = 'Выгода: ' + job.reward + '₽';
+        }
         jobNode.appendChild(jobRewardNode);
         jobsNode.appendChild(jobNode);
     });
