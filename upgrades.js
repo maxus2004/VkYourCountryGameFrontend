fa351d48aecaf7a1a0f5671a2c5b0461e372243b
Дорошин Максим <maxus2004123@gmail.com>
2021-04-09 23:04:01 +0300

    update text

--- upgrades.js
+++ upgrades.js
@@ -1,10 +1,10 @@
 var jobs = [
-    { name: 'Сдавать металлолом', cost: 0, reward: 50, rewardDelay: 1, repeating: false, maxAmount: 1},
-    { name: 'Попрошайничать', cost: 0, reward: 50, rewardDelay: 1, repeating: false, maxAmount: 1},
-    { name: 'Работать на складе', cost: 0, reward: 5000, rewardDelay: 3, repeating: false, maxAmount: 1},
-    { name: 'Продавать мороженное', cost: 500, reward: 2000, rewardDelay: 1, repeating: false, maxAmount: 1},
-    { name: 'Открыть магазин', cost: 50000, reward: 200000, rewardDelay: 30, repeating: true, maxAmount: 20},
-    { name: 'Стать президентом', cost: 100000000, reward: 10000000000, rewardDelay: 365, repeating: false, maxAmount: 1},
+    { name: 'Сдавать металлолом', cost: 0, reward: 50, rewardDelay: 1, repeating: false, maxAmount: 1 },
+    { name: 'Попрошайничать', cost: 0, reward: 50, rewardDelay: 1, repeating: false, maxAmount: 1 },
+    { name: 'Работать на складе', cost: 0, reward: 5000, rewardDelay: 3, repeating: false, maxAmount: 1 },
+    { name: 'Продавать мороженное', cost: 500, reward: 2000, rewardDelay: 1, repeating: false, maxAmount: 1 },
+    { name: 'Открыть магазин', cost: 50000, reward: 200000, rewardDelay: 30, repeating: true, maxAmount: 20 },
+    { name: 'Стать президентом', cost: 100000000, reward: 10000000000, rewardDelay: 365, repeating: false, maxAmount: 1 },
 ]
 
 function numToDays(num) {
@@ -29,11 +29,14 @@ function loadJobs() {
         jobNode.appendChild(jobNameNode);
         var jobCostNode = document.createElement('p');
         jobCostNode.className = 'jobCost';
-        jobCostNode.innerText ='Цена: '+job.cost + '₽';
+        jobCostNode.innerText = 'Цена: ' + job.cost + '₽';
         jobNode.appendChild(jobCostNode);
         var jobRewardNode = document.createElement('p');
         jobRewardNode.className = 'jobReward';
-        jobRewardNode.innerText = 'Выгода: '+job.reward + '₽ через ' + job.rewardDelay + ' ' + numToDays(job.rewardDelay);
+        if (job.rewardDelay > 0)
+            jobRewardNode.innerText = 'Выгода: ' + job.reward + '₽ через ' + job.rewardDelay + ' ' + numToDays(job.rewardDelay);
+        else
+            jobRewardNode.innerText = 'Выгода: ' + job.reward + '₽';
         jobNode.appendChild(jobRewardNode);
         jobsNode.appendChild(jobNode);
     });
