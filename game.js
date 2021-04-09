d38bac1c90796c35b63852fad8c89b58facbb99e
Дорошин Максим <maxus2004123@gmail.com>
2021-04-09 22:14:12 +0300

    add connection to server

---
+++ game.js
@@ -0,0 +1,21 @@
+function loadPlayerData() {
+    bridge.send("VKWebAppGetUserInfo").then(data => {
+        console.log(data);
+        document.getElementById("name").innerText = data.first_name+" "+data.last_name;
+        document.getElementById("userPicture").src = data.photo_200;
+    });
+
+    let response = await fetch("http://servermaksa.tk/yourcountryserver/getUser",
+        {
+            method: "POST",
+            body: JSON.stringify({ id: document.location.hash.vk_user_id })
+        });
+
+    if (response.ok) {
+        let json = await response.json();
+        console.log(json);
+    } else {
+        console.log(response);
+    }
+
+}
