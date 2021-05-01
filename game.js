f88ab5d36f554d6e81177c9da13d96eabc105e97
Дорошин Максим <maxus2004123@gmail.com>
2021-05-01 20:00:53 +0300

    fix

--- game.js
+++ game.js
@@ -9,6 +9,7 @@ async function loadPlayerData() {
     result = await request.json();
     playerData = result.playerData;
     result.activeTasks.forEach(taskId => {
+        tasks[jobId].active = true;
         document.getElementById('upgrades').children[taskId].classList.add('jobActive');
     });
 
